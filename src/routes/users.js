70e77e5c579a00d509839b11fcb02b09537d9abf
rofa <zainrofa29@gmail.com>
2022-11-03 02:55:50 +0700

    fixing

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,11 +5,11 @@ const isLogin = require("../middleware/isLogin");
 const allowedRoles = require("../middleware/allowedRoles");
 const uploads = require("../middleware/upload");
 
-usersRouter.get("/profile_user", allowedRoles("User"), get);
+usersRouter.get("/profile_user", isLogin(), allowedRoles("User"), get);
 // register
 usersRouter.post("/", create);
 // edit profile
-usersRouter.patch("/profile", allowedRoles("User"), uploads, edit);
+usersRouter.patch("/profile", isLogin(), allowedRoles("User"), uploads, edit);
 // edit password
 usersRouter.patch("/password/:id", editPassword);
 
